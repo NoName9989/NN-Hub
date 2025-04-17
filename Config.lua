10282bba8bea3127d9cee87e81797e43beac80aa
NoName9989 <130733726+NoName9989@users.noreply.github.com>
2025-04-17 23:45:01 +0700

    Create Config.lua

---
+++ Config.lua
@@ -0,0 +1,8 @@
+return {
+    UseKeySystem = false, -- Người dùng bật nếu cần
+    KeySystem = {
+        KeyLink = "https://luarmor.net/keysystem/yourhub",
+        APIEndpoint = "https://yourapi.com/verify?hwid=",
+        HWIDMethod = "UserId", -- hoặc "RandomGUID"
+    }
+}
