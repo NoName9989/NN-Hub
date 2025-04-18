d22e8f1eba49e6afe79e36c072088d44171cc33d
NoNameAKA <130733726+NoName9989@users.noreply.github.com>
2025-04-18 10:59:47 +0700

    Delete Config.lua

--- Config.lua
+++
@@ -1,8 +0,0 @@
-return {
-    UseKeySystem = false, -- Người dùng bật nếu cần
-    KeySystem = {
-        KeyLink = "https://luarmor.net/keysystem/yourhub",
-        APIEndpoint = "https://yourapi.com/verify?hwid=",
-        HWIDMethod = "UserId", -- hoặc "RandomGUID"
-    }
-}
